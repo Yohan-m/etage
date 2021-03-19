77189b3cc57dec9a015fc8b37a0f1ae5bc1e4758
Yohan Marchand <yohan.marchand3@gmail.com>
2021-03-19 11:30:27 +0100

    restore

--- 7670b40ec595bc4e3eeb8af733d2afafb1d56a4ed22346da95536ba3cf35.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="7670b40ec595bc4e3eeb8af733d2afafb1d56a4ed22346da95536ba3cf35" tilewidth="32" tileheight="32" tilecount="200" columns="25">
- <image source="../../../Téléchargements/7670b40ec595bc4e3eeb8af733d2afafb1d56a4ed22346da95536ba3cf35.png" width="800" height="256"/>
-</tileset>
